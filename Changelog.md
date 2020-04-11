5916738bd38bf27a33e11549725a6c70c000b992
MarioMoura <mario@mariomoura.com>
2020-04-11 20:43:18 -0300

    version 1.2

---
+++ Changelog.md
@@ -0,0 +1,15 @@
+# 1.2 (April 10 2020)
+
+- Debug mode added
+- Reading from a file added
+
+# 1.1 (April 3 2020)
+
+- Pipe Implemented
+
+# 1.0 (Feb 28 2020)
+
+- Just the bare minimum
+- No pipes 
+- No redirect
+- Almost no shell sintaxe
--- main.c
+++ main.c
@@ -1,5 +1,16 @@
-// A simple, educational program of how a shell
-// (more or less) works
+/* A simple, educational program of how a shell
+ * (more or less) works
+ *
+ * TODO: - Redict of output
+ *       - Indirect of input
+ *       - Prompt
+*/
+
+/*
+==============================
+= Includes
+==============================
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,18 +18,39 @@
 #include <string.h>
 #include <sys/wait.h>
 
-//#define DEBUG 1 
-
 #define ARGSIZE 32
+
+/* definitions that help with the
+   pipes filedescriptors
+*/
 #define READ 0
 #define WRITE 1
 
-void printargs(char **args){
+#ifdef DEBUG
+#endif
+
+/*
+==============================
+= Functions
+==============================
+*/
+
+/* used to print the array of
+ * strings, generally used in
+ * debug
+ */
+void printargs   (char **args){
+	#ifdef DEBUG
+	printf("Command: ");
+	#endif
 	for(int i=0; args[i] != NULL; i++)
-		puts(args[i]);
+		printf("%s ", args[i]);
+	printf("\n");
 }
-// transforms buf into a more readable form
-// much like argv array of main
+/* transforms buf into a more readable form
+ * much like argv array of main, ready to 
+ * be passed to exec*
+ */
 void toarray(char *buf, char  *args[]){
 	memset(args, 0, ARGSIZE);
 	char inword = 0;
@@ -27,6 +59,10 @@ void toarray(char *buf, char  *args[]){
 	for(int i=0; i < len; i++){
 		switch(buf[i]){
 			case ' ' :
+				inword = 0;
+				buf[i] = 0;
+				break;
+			case '\n' :
 				inword = 0;
 				buf[i] = 0;
 				break;
@@ -42,14 +78,15 @@ void toarray(char *buf, char  *args[]){
 				break;
 			default :
 				if(inword == 0)
-					args[wordc++] = &(buf[i]);	
+					args[wordc++] = &(buf[i]);
 				inword = 1;
 				break;
 		}
 	}
 }
-// read args between the pipes n and n+1  and returns another
-// array the formated command as well as the prefered output
+/* read args between the pipes n and n+1  and returns another
+ * array the formated command as well as the prefered output
+ */
 char **parse_pipe(char **array, int n, char **out_f){
 	int npipes = 0;
 	int size = 1;
@@ -82,7 +119,9 @@ char **parse_pipe(char **array, int n, char **out_f){
 	cmd[size-1] = NULL;
 	return cmd;	
 }
-// count the number of pipes in the command
+/* count the number of pipes in the command
+ * generally the hole array
+ */
 int count_pipe(char **array){
 	int npipe = 0;
 	for(int i=0; array[i] != NULL; i++)
@@ -90,10 +129,11 @@ int count_pipe(char **array){
 			npipe++;
 	return npipe;
 }
-// fork and execute the program arg[0] with the
-// arguments args[1...n] having "in" as stdin and
-// "out" as stdout
-// basically a helper function
+/* fork and execute the program arg[0] with the
+ * arguments args[1...n] having "in" as stdin and
+ * "out" as stdout
+ * basically a helper function
+ */
 int shell_exec(char *args[], int in, int out){
 	if(fork() == 0){
 		if( in != fileno(stdin)){
@@ -112,6 +152,12 @@ int shell_exec(char *args[], int in, int out){
 		return 0;
 	}	
 }
+/* This is the big boy, array is
+ * the array of strings that is 
+ * the command, and out_f is
+ * a pointer to the file redirect
+ * in case of one
+ */
 int run(char **array, char **out_f){
 	int in_save = dup(0);
 	int fd[2];
@@ -120,9 +166,9 @@ int run(char **array, char **out_f){
 	char **args;
 	for( i=0; i < loop ; i++){
 		pipe(fd);
-		args = parse_pipe(array, i, out_f);
+		args = parse_pipe(array, i, out_f); // alloc
 		shell_exec(args, in, fd[WRITE]);
-		free(args);
+		free(args);// free
 		close(fd[WRITE]);
 		in = fd[READ];
 	}
@@ -132,17 +178,21 @@ int run(char **array, char **out_f){
 	shell_exec(args, in, fileno(stdout));
 	free(args);
 	dup2(in_save, 0);
+	return 0;
 }
+/*
+
+ *
+ */
+#ifndef DEBUG
 int main(void){
 	char buf[256];// stdin buffer
 	char *array[ARGSIZE] = {0}; // arguments for the program
-	char *prompt = "shell>>";
+	char *prompt = "protsh>> ";
 	char **args;
 
-	char *out_f = 0;
-	int out = fileno(stdout);
+	char *out_f = 0; // supposed to be path to the redirect file
 
-	#ifndef DEBUG
 	while(1){ // begin of the shell loop
 		printf("%s",prompt); // print the prompt
 		if(scanf(" %[^\n]", buf) != 1){ // input
@@ -155,18 +205,39 @@ int main(void){
 		if(!strcmp(array[0],"exit")) // exits is "exit" is typed
 			exit(0);
 
-		args = parse_pipe(array, 0, &out_f);
-
 		run( array, &out_f);
 		
-		free(args);
 	}
-	#else
-	puts("debuging...");
-	strcpy(buf, "ls -l | sort | wc -l ");
-	toarray(buf, array);
-	run( array, &out_f);
-	puts("eae man");
-	#endif
 	return 0;
 }
+#else
+/* 
+==============================
+= Debug mode
+==============================
+*/
+int main(int argc, char *argv[]){
+	char buf[256];
+	char *array[ARGSIZE] = {0};
+	char *out_f = 0;
+	FILE *ifile;
+
+	puts("Single execution mode");
+
+	if( argc == 1){
+		strcpy(buf, "ls -lah");
+	}else{
+		printf("Opening \"%s\"...\n", argv[1] );
+		if ( (ifile = fopen(argv[1],"r")) != NULL ){
+			fread(buf, sizeof buf, 1, ifile); 
+		}else{
+			fprintf(stderr, "could not open file \"%s\"", argv[1]);
+			return 1;
+		}
+	}
+	toarray(buf, array);
+	printargs(array);
+	run(array, &out_f); 
+	return 0;
+}
+#endif
